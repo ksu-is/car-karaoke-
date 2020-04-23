22871309a0fe68cb9ddd3c87d777305c6d504dcf
Bailey Sung Bae <51390523+baechugongdue@users.noreply.github.com>
2020-04-23 11:44:10 +0900

    Update Alexa_siwmp.py

--- Alexa_siwmp.py
+++ Alexa_siwmp.py
@@ -5,9 +5,8 @@
 from time import sleep
 import RPi.GPIO as GPIO
 from pi_sht1x import SHT1x
-from hackeriet.mqtt import MQTT
 
-def main():
+def moisture():
     mqtt = MQTT() 
     mode = GPIO.BCM
     with SHT1x(12, 8, vdd="5V") as sensor_one:
@@ -15,9 +14,6 @@ def main():
         humidity = sensor_one.read_humidity(temp)
         sensor_one.calculate_dew_point(temp, humidity)
         print(sensor_one)
-        mqtt("hackeriet/plant1/temperature", temp)
-        mqtt("hackeriet/plant1/humidity", humidity)
-        f = open("/home/pi/plant_mon/plant1", "w")
         f.write("%s," % str(temp))
         f.write("%s\n" % str(humidity))
         f.close()
@@ -41,3 +37,5 @@ if __name__ == "__main__":
     
     
  #This is code to send the data from raspberry pi to alexa
+
+
