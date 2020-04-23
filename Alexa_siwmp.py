1a1a9575b12b73c04fdf5b488c9ab15acf467dfa
Bailey Sung Bae <51390523+baechugongdue@users.noreply.github.com>
2020-04-23 12:12:32 +0900

    Update Alexa_siwmp.py

--- Alexa_siwmp.py
+++ Alexa_siwmp.py
@@ -69,41 +69,8 @@ while True:
     time.sleep(1)
 
 
-from time import sleep
-import RPi.GPIO as GPIO
-from pi_sht1x import SHT1x
-
-def moisture():
-    mqtt = MQTT() 
-    mode = GPIO.BCM
-    with SHT1x(12, 8, vdd="5V") as sensor_one:
-        temp = sensor_one.read_temperature()
-        humidity = sensor_one.read_humidity(temp)
-        sensor_one.calculate_dew_point(temp, humidity)
-        print(sensor_one)
-        f.write("%s," % str(temp))
-        f.write("%s\n" % str(humidity))
-        f.close()
-        sleep(2)
-
-    with SHT1x(18, 16, vdd="5V") as sensor_two:
-        temp = sensor_two.read_temperature()
-        humidity = sensor_two.read_humidity(temp)
-        sensor_two.calculate_dew_point(temp, humidity)
-        mqtt("hackeriet/plant0/temperature", temp)
-        mqtt("hackeriet/plant0/humidity", humidity)
-        f = open("/home/pi/plant_mon/plant0", "w")
-        f.write("%s," % str(temp))
-        f.write("%s\n" % str(humidity))
-        f.close()
-        print(sensor_two)
-        sleep(2)
-
-if __name__ == "__main__":
-    main()
     
-    
- #This is code to send the data from raspberry pi to alexa
+#This is code to send the data from raspberry pi to alexa
 #logging into alexa 
 logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s')
 coloredlogs.DEFAULT_FIELD_STYLES = {
